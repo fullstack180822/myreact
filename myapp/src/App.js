b8910c6f9c95e69a2f8722500686514c35b3e0d2
micserv010821 <dotnet290320@gmail.com>
2023-06-08 20:31:12 +0300

    upload

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,24 @@
-
 import './App.css';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
+import Post from './components/Post'
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <h1>--Start Your App--</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <header className="App">
+        <Navbar />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/posts/:post_id' component={Post} />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
